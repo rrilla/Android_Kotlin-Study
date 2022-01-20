5f57333ec24efb25e2685214c0318286ac139a0a
rrilla <rrilla@naver.com>
2022-01-20 14:02:47 +0900

    feat: View관련 학습
    - View 바인딩
    - Code로 View 객체 생성하여 화면구성

--- AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.kt
+++ AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.androidlab
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.androidlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        뷰 바인딩
+        val binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        binding.test.text = "asdsdfasd"
+        binding.test2.setText("zzzzzzzzzzz")
+
+//       code로 view 생성
+        val name = TextView(this).apply {
+            typeface = Typeface.DEFAULT
+            text = "afsdf"
+        }
     }
 }
---
+++ AndroidLab/app/src/main/java/com/example/androidlab/Test.kt
@@ -0,0 +1,9 @@
+package com.example.androidlab
+
+fun main() {
+    println("hello jaehyeon")
+}
+
+class Test {
+    val asdf:String = """asdf"""
+}
