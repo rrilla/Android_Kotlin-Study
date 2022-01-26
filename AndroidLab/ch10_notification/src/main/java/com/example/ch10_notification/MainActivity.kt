29e6690d2ef13c342b54a5a22a1029d21ece05dc
rrilla <rrilla@naver.com>
2022-01-26 23:53:34 +0900

    feat: 권한 확인, 요청 my sample 생성중

--- AndroidLab/ch10_notification/src/main/java/com/example/ch10_notification/MainActivity.kt
+++ AndroidLab/ch10_notification/src/main/java/com/example/ch10_notification/MainActivity.kt
@@ -1,31 +1,109 @@
 package com.example.ch10_notification
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.ch10_notification.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         permissionGranted()
     }
 
     private fun permissionGranted(): Boolean {
-        //  퍼미션 허용 확인 함수
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            //  퍼미션 요청 함수
-            ActivityCompat.requestPermissions(this, arrayOf<String>("android.permission.ACCESS_FINE_LOCATION"), 100)
-            return false
-        }else{
-            return true
+//        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            when {
+//                ContextCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.ACCESS_FINE_LOCATION
+//                ) == PackageManager.PERMISSION_GRANTED -> {
+//                    // You can use the API that requires the permission.
+//                    Log.e("han", "권한관련 기능 사용")
+//                    true
+//                }
+//                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+//                    val snackBar = Snackbar.make(binding.layout, R.string.suggest_permissison_grant_in_setting, Snackbar.LENGTH_INDEFINITE)
+//                    snackBar.setAction("확인") {
+//                        val intent = Intent()
+//                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+//                        val uri = Uri.fromParts("package", packageName, null)
+//                        intent.data = uri
+//                        startActivity(intent)
+//                    }
+//                    snackBar.show()
+//
+//                    false
+//                }
+//                // In an educational UI, explain to the user why your app requires this
+//                // permission for a specific feature to behave as expected. In this UI,
+//                // include a "cancel" or "no thanks" button that allows the user to
+//                // continue using your app without granting the permission.
+//                else -> {
+//                    // You can directly ask for the permission.
+//                    ActivityCompat.requestPermissions(this,
+//                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                        100)
+//                    false
+//                }
+//            }
+//        } else {
+//            TODO("VERSION.SDK_INT < M")
+//        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                val snackBar = Snackbar.make(
+                    binding.layout,
+                    R.string.suggest_permissison_grant,
+                    Snackbar.LENGTH_INDEFINITE
+                )
+                snackBar.setAction("권한승인") {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        100
+                    )
+                }
+                snackBar.show()
+            } else {
+                val snackBar = Snackbar.make(
+                    binding.layout,
+                    R.string.suggest_permissison_grant_in_setting,
+                    Snackbar.LENGTH_INDEFINITE
+                )
+                    snackBar.setAction("확인") {
+                        val intent = Intent()
+                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                    }
+                    snackBar.show()
+            }
         }
+        return true
     }
 
 //    퍼미션 요청 다이얼로그 닫힐 때 자동 호출
@@ -37,4 +115,5 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         Log.e("Han", "requestCode : $requestCode, permissions : ${permissions.contentToString()}, grantResults : ${Arrays.toString(grantResults)}")
     }
+
 }
