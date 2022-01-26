e6d59919825248aad4e6d6414ffcb4c895d4c566
rrilla <rrilla@naver.com>
2022-01-27 04:51:06 +0900

    feat: 권한 확인, 요청 my sample 완성
    - 권한 승인 여부 체크
    - 권한 거부시 해당 기능 사용 불가 멘트, 그에다른 기능 추가

--- AndroidLab/ch10_notification/src/main/java/com/example/ch10_notification/MainActivity.kt
+++ AndroidLab/ch10_notification/src/main/java/com/example/ch10_notification/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.example.ch10_notification
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.ch10_notification.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
-import java.util.*
+import kotlin.random.Random
+
+const val REQUEST_CODE = 100
 
 class MainActivity : AppCompatActivity() {
 
+    //  바인딩 객체 전역변수로 선언
     private var mBinding: ActivityMainBinding? = null
-    private val binding get() = mBinding!!
+    //  !! : 객체가 null일 때 예외 발생시킴
+    //  lazy, get()없이 초기화시 null값이 먼저 대입되어 빌드시 에러
+    //  private val binding get() = mBinding
+    private val binding by lazy {mBinding!!}
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,21 +40,38 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        permissionGranted()
+        if(isPermissionGranted()) {
+            getCurrentLocation()
+        }
     }
 
-    private fun permissionGranted(): Boolean {
-//        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            when {
-//                ContextCompat.checkSelfPermission(
-//                    this,
-//                    Manifest.permission.ACCESS_FINE_LOCATION
-//                ) == PackageManager.PERMISSION_GRANTED -> {
-//                    // You can use the API that requires the permission.
-//                    Log.e("han", "권한관련 기능 사용")
-//                    true
-//                }
-//                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+    //  isPermissionGranted()함수 내부에서 권한 체크를 하였으나
+    //  사용자가 거부할 권한이 필요하다는 경고 무시 위해 추가
+    @SuppressLint("MissingPermission")
+    fun getCurrentLocation() {
+        Log.e("han","1");
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+                location: Location? ->
+            Log.e("han","2");
+            if (location != null) {
+                binding.tvLocation.text = "위도: ${ location.latitude + Random.nextInt() % 15} / 경도: ${location.longitude + Random.nextInt() % 15}"
+            } else
+                binding.tvLocation.text = "위치를 알 수 없습니다."
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        Log.e("han","실행됨");
+        return when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    true
+                }
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
 //                    val snackBar = Snackbar.make(binding.layout, R.string.suggest_permissison_grant_in_setting, Snackbar.LENGTH_INDEFINITE)
 //                    snackBar.setAction("확인") {
 //                        val intent = Intent()
@@ -50,70 +81,48 @@ class MainActivity : AppCompatActivity() {
 //                        startActivity(intent)
 //                    }
 //                    snackBar.show()
-//
-//                    false
-//                }
-//                // In an educational UI, explain to the user why your app requires this
-//                // permission for a specific feature to behave as expected. In this UI,
-//                // include a "cancel" or "no thanks" button that allows the user to
-//                // continue using your app without granting the permission.
-//                else -> {
-//                    // You can directly ask for the permission.
-//                    ActivityCompat.requestPermissions(this,
-//                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-//                        100)
-//                    false
-//                }
-//            }
-//        } else {
-//            TODO("VERSION.SDK_INT < M")
-//        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            ) {
-                val snackBar = Snackbar.make(
-                    binding.layout,
-                    R.string.suggest_permissison_grant,
-                    Snackbar.LENGTH_INDEFINITE
-                )
-                snackBar.setAction("권한승인") {
-                    ActivityCompat.requestPermissions(
-                        this,
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        100
-                    )
-                }
-                snackBar.show()
-            } else {
-                val snackBar = Snackbar.make(
-                    binding.layout,
-                    R.string.suggest_permissison_grant_in_setting,
-                    Snackbar.LENGTH_INDEFINITE
-                )
-                    snackBar.setAction("확인") {
-                        val intent = Intent()
-                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                        val uri = Uri.fromParts("package", packageName, null)
-                        intent.data = uri
-                        startActivity(intent)
+
+                    AlertDialog.Builder(this).run {
+//                        setTitle("위치권한확인")
+                        setIcon(android.R.drawable.ic_dialog_alert)
+                        setMessage(R.string.suggest_permissison_grant_in_setting)
+                        setPositiveButton("확인"){
+                            _, _ ->
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts("package", packageName, null)
+                            intent.data = uri
+                            startActivity(intent)
+                        }
+                        setNegativeButton("취소", null)
+                        show()
                     }
-                    snackBar.show()
+                    false
+
+                }
+                else -> {
+                    Log.e("han","실행됨2");
+                    ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        REQUEST_CODE)
+                    false
+                }
             }
         }
-        return true
-    }
 
-//    퍼미션 요청 다이얼로그 닫힐 때 자동 호출
+    //  퍼미션 요청 다이얼로그 닫힐 때 자동 호출
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        Log.e("Han", "requestCode : $requestCode, permissions : ${permissions.contentToString()}, grantResults : ${Arrays.toString(grantResults)}")
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            } else {
+                Toast.makeText(this, R.string.no_permission_msg, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-
 }
